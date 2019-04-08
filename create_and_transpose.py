3adaef9118e10d5b51346435c77b4683c97db1b3
senatn <senatan91@gmail.com>
2019-04-08 23:47:16 +0300

    Create create_and_transpose.py

---
+++ create_and_transpose.py
@@ -0,0 +1,15 @@
+import random
+print("Create and transpose a random matrix!\nPlease enter the following information. ")
+row = int(input("Rows\t: "))
+column = int(input("Columns\t: "))
+From = int(input("From\t: "))
+To = int(input("To\t\t: "))
+print("\n")
+
+mRandom = [[i for i in (random.randint(From,To) for i in range(row))] for i in range(column)]
+for i in mRandom:
+    print(i)
+print("\n")
+TmRandom = [[mRandom[i][j] for i in range(column)] for j in range(row)]
+for i in TmRandom:
+    print(i)
