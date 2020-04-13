72e68cc6a9ec1d47d2f4bad4b02d478f74a9dcae
sena <46344611+senatn@users.noreply.github.com>
2020-04-14 01:23:57 +0300

    Update create_and_transpose.py

--- create_and_transpose.py
+++ create_and_transpose.py
@@ -6,10 +6,10 @@ From = int(input("From\t: "))
 To = int(input("To\t\t: "))
 print("\n")
 
-mRandom = [[i for i in (random.randint(From,To) for i in range(row))] for i in range(column)]
+mRandom = [[i for i in (random.randint(From,To) for i in range(column))] for i in range(row)]
 for i in mRandom:
     print(i)
 print("\n")
-TmRandom = [[mRandom[i][j] for i in range(column)] for j in range(row)]
+TmRandom = [[mRandom[i][j] for i in range(row)] for j in range(column)]
 for i in TmRandom:
     print(i)
